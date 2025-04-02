5274c4f6df20042c25816a711ae33b214e0f8f25
MoGit10 <116606731+MoGit10@users.noreply.github.com>
2025-04-02 16:57:50 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,11 @@ def print_message():
 
 def print_greeting_message():
     print("How are you?")
+
+def print_nice_greeting():
+    print("Hello, I am well nice greeting ")
+
+
+print_nice_greeting()
+print_message()
+print_greeting_message()
